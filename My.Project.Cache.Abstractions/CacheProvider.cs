71d7976ddd13f550cc048cecd49b10d1dbf4db9f
liuzhixin405 <164910441@qq.com>
2022-09-14 23:55:52 +0800

    u
    u

---
+++ My.Project.Cache.Abstractions/CacheProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Project.Cache.Abstractions
+{
+    /// <summary>
+    /// 缓存提供器
+    /// </summary>
+    public enum CacheProvider
+    {
+        /// <summary>
+        /// 内存缓存
+        /// </summary>
+        [Description("内存缓存")]
+        MemoryCache,
+        /// <summary>
+        /// Redis
+        /// </summary>
+        [Description("Redis")]
+        Redis
+    }
+}
