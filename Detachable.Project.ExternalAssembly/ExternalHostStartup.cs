7b4d2eaa0c3792e24b5dd9f4b21d1b12a8665f19
liuzhixin405 <164910441@qq.com>
2022-09-24 21:09:58 +0800

    u
    u

--- Detachable.Project.ExternalAssembly/ExternalHostStartup.cs
+++ Detachable.Project.ExternalAssembly/ExternalHostStartup.cs
@@ -1,5 +1,4 @@
-﻿using Detachable.Project.ExternalAssembly.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
